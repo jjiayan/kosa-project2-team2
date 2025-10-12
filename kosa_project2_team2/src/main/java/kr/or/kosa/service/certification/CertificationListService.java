70444c31ec5e58b7faf473ac42df58479ae294e5
unknown <blackteatime4@naver.com>
2025-10-12 13:44:02 +0900

    comment/상세 주석 추가

--- kosa_project2_team2/src/main/java/kr/or/kosa/service/certification/CertificationListService.java
+++ kosa_project2_team2/src/main/java/kr/or/kosa/service/certification/CertificationListService.java
@@ -1,13 +1,36 @@
 package kr.or.kosa.service.certification;
 
 import java.util.List;
-
 import kr.or.kosa.dao.CertificationDao;
 import kr.or.kosa.dto.Certification;
 
+/** [자격증 전체 목록 조회]
+ * 
+ * ✔ 책임(SRP)
+ *  - DAO를 호출하여 전체 자격증 리스트를 반환한다.
+ *  - 추가적인 비즈니스 로직은 포함하지 않는다.
+ * 
+ * ✔ 예외 처리
+ *  - 이 메서드는 Exception을 던진다.
+ *  - 실제 예외 처리(try-catch)는 Action(컨트롤러)에서 수행한다.
+ * 
+ * ✔ 확장성
+ *  - 추후 페이징 / 검색 / 정렬 등으로 확장할 수 있다.
+ */
 public class CertificationListService {
+
+    private final CertificationDao certificationDao;
+
+    public CertificationListService() {
+        this.certificationDao = new CertificationDao();
+    }
+
+    /**
+     * 전체 자격증 목록 조회
+     * @return List<Certification>
+     * @throws Exception DB 조회 실패 시
+     */
     public List<Certification> getCertificationList() throws Exception {
-        CertificationDao dao = new CertificationDao();
-        return dao.getAllCertifications();
+        return certificationDao.getAllCertifications();
     }
 }
