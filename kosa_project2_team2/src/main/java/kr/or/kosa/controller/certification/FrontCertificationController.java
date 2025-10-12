06616442588be857abead30b7009eca7eba88d5e
unknown <blackteatime4@naver.com>
2025-10-12 14:11:54 +0900

    feat/만들어진 컨트롤러들에 알맞게 frontController에 반영

--- kosa_project2_team2/src/main/java/kr/or/kosa/controller/certification/FrontCertificationController.java
+++ kosa_project2_team2/src/main/java/kr/or/kosa/controller/certification/FrontCertificationController.java
@@ -1,7 +1,6 @@
 package kr.or.kosa.controller.certification;
 
-import java.io.IOException;
-
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -9,27 +8,17 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import kr.or.kosa.action.Action;
 import kr.or.kosa.action.ActionForward;
-import kr.or.kosa.controller.certification.ajax.CertificationAjaxController;
+
+import java.io.IOException;
 
 @WebServlet("*.cert")
 public class FrontCertificationController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	@Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        doProcess(request, response);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        doProcess(request, response);
-    }
-
-    private void doProcess(HttpServletRequest request, HttpServletResponse response)
+	private void doProcess(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+        // 1) 요청 URI 및 ContextPath 분리
         String requestUri = request.getRequestURI();
         String contextPath = request.getContextPath();
         String urlCommand = requestUri.substring(contextPath.length());
@@ -39,21 +28,50 @@ public class FrontCertificationController extends HttpServlet {
         Action action = null;
         ActionForward forward = null;
 
-        if(urlCommand.equals("/certificationList.cert")) {
-            action = new CertificationAjaxController();    // 화면 또는 DB처리 포함
-            forward = action.execute(request, response);
+        if (urlCommand.equals("/certificationList.cert")) {
+            action = new CertificationListController();
 
-        } else if(urlCommand.equals("/certificationAjax.cert")) {
-            action = new CertificationAjaxController();     // JSON 응답
+        } else if (urlCommand.equals("/certificationDetail.cert")) {
+            action = new CertificationDetailController();
+
+        } else if (urlCommand.equals("/certificationAjax.cert")) {
+            action = new CertificationAjaxController();
+
+        } else if (urlCommand.equals("/certificationSync.cert")) {
+            action = new CertificationSyncController();
+
+        } else {
+            // 404 or 에러 페이지
+            forward = new ActionForward();
+            forward.setRedirect(false);
+            forward.setPath("/WEB-INF/views/error.jsp");
+        }
+
+        // Action 실행
+        if (action != null) {
             forward = action.execute(request, response);
         }
 
-        if(forward != null) {
-            if(forward.isRedirect()) {
+        // forward or redirect
+        if (forward != null) {
+            if (forward.isRedirect()) {
                 response.sendRedirect(forward.getPath());
             } else {
-                request.getRequestDispatcher(forward.getPath()).forward(request, response);
+                RequestDispatcher dis = request.getRequestDispatcher(forward.getPath());
+                dis.forward(request, response);
             }
         }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doProcess(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doProcess(request, response);
     }
 }
