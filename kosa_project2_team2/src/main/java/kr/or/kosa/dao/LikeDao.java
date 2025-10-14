168a7e079ad423b2b8210b72723888fa281daac5
chingudl7 <chingudl7@naver.com>
2025-10-14 15:11:28 +0900

    좋아요 DAO: 좋아요 존재 여부 확인, 좋아요 추가, 삭제, 좋아요 수 조회, 게시글 좋아요 사용자 목록 조회 작성.

--- kosa_project2_team2/src/main/java/kr/or/kosa/dao/LikeDao.java
+++ kosa_project2_team2/src/main/java/kr/or/kosa/dao/LikeDao.java
@@ -2,15 +2,43 @@ package kr.or.kosa.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import kr.or.kosa.dto.LikeDto;
+import kr.or.kosa.dto.ReplyDto;
 import kr.or.kosa.utils.ConnectionPoolHelper;
 
 public class LikeDao {
 	// 1. 좋아요 존재 여부 확인
-	public boolean existsLike(LikeDto like) {
-		return true;
+	public boolean checkIsLiked(LikeDto like) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		boolean isLiked = false;
+		
+		try {
+			conn = ConnectionPoolHelper.getConnection();
+			String sql = "SELECT COUNT(*) AS CNT FROM " + like.getTableName() + 
+                    " WHERE " + like.getTargetColumnName() + " = ? AND USER_ID = ?";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setLong(1, like.getTargetId());
+            pstmt.setLong(2, like.getUserId());
+			rs = pstmt.executeQuery();
+			
+			if (rs.next()) {
+				isLiked = rs.getInt(1) > 0;
+			}
+			
+		} catch (Exception e) {
+			
+		} finally {
+			ConnectionPoolHelper.close(pstmt);
+			ConnectionPoolHelper.close(conn);
+		}
+		return isLiked;
 	}
 	
 	// 2. 좋아요 추가
@@ -42,16 +70,98 @@ public class LikeDao {
 	
 	// 3. 좋아요 삭제
 	public int deleteLike(LikeDto like) {
-		return 0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int row = 0;
+		
+		try {
+			conn = ConnectionPoolHelper.getConnection();
+			
+			String sql = "DELETE FROM " + like.getTableName() +
+						 " WHERE " + like.getTargetColumnName() + " = ? and user_id = ?";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setLong(1, like.getTargetId());
+			pstmt.setLong(2, like.getUserId());
+			
+			row = pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			ConnectionPoolHelper.close(pstmt);
+			ConnectionPoolHelper.close(conn);
+		}
+		
+		return row;
 	}
 	
 	// 4. 좋아요 개수 조회
 	public int getLikeCnt(LikeDto like) {
-		return 0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		int count = 0;
+		
+		try {
+			conn = ConnectionPoolHelper.getConnection();
+			String sql = "SELECT COUNT(*) AS CNT FROM " + like.getTableName() +
+						 " WHERE " + like.getTargetColumnName() + " = ?";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setLong(1, like.getTargetId());
+			rs = pstmt.executeQuery();
+			
+			if (rs.next()) {
+				count = rs.getInt("CNT");
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			ConnectionPoolHelper.close(rs);
+			ConnectionPoolHelper.close(pstmt);
+			ConnectionPoolHelper.close(conn);
+		}
+		return count;
 	}
 	
-	// 5. 게시글 좋아요 상세정보 조회(사용자 목록) 
+	// 5. 게시글 좋아요 상세정보 조회(사용자 목록) - 쿼리문 수정 필요 
 	public List<LikeDto> likeListByRoomBoardId(LikeDto like){
-		return null;
+			
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		List<LikeDto> list = new ArrayList<>();
+		
+		try {
+			conn = ConnectionPoolHelper.getConnection();
+			String sql = "SELECT L.USER_ID, L.ROOM_BOARD_ID, L.LIKE_CREATED_AT, U.USER_PHOTO, U.USER_NICKNAME, U.USER_STATUS " +
+						 "FROM LIKE_ROOM_BOARD L INNER JOIN \"USER\" U ON L.USER_ID = U.USER_ID " +
+						 "WHERE L.ROOM_BOARD_ID = ? AND U.USER_STATUS = 'ACTIVE'" +
+						 "ORDER BY L.LIKE_CREATED_AT DESC";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setLong(1, like.getTargetId());
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				LikeDto likes = new LikeDto();
+				likes.setUserId(rs.getLong("USER_ID"));
+	            likes.setUserNickname(rs.getString("USER_NICKNAME"));
+	            likes.setUserPhoto(rs.getString("USER_PHOTO"));
+	            likes.setLikeCreatedAt(rs.getTimestamp("LIKE_CREATED_AT"));
+	            list.add(likes);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			ConnectionPoolHelper.close(rs);
+			ConnectionPoolHelper.close(pstmt);
+			ConnectionPoolHelper.close(conn);
+		}
+		
+		return list;
 	}
 }
